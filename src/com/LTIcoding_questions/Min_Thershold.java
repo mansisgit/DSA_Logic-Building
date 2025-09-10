b9173644aad128d6dc4abd230d7bcf989aeb06e8
mansisgit <mancc81@gmail.com>
2025-09-10 18:28:57 +0530

    done

--- src/com/LTIcoding_questions/Min_Thershold.java
+++ src/com/LTIcoding_questions/Min_Thershold.java
@@ -1,4 +1,23 @@
 package com.LTIcoding_questions;
 
 public class Min_Thershold {
+
+    public static void thresholdCount(String s, int threshold){
+        int [] count = new int[256];
+
+        for(char c : s.toCharArray()) count[c]++;
+
+        for(int i =0;i<256;i++){
+            if(count[i]>=threshold){
+                System.out.println((char) i + ":"+count[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "apple and banaana";
+        int thr = 3;
+        thresholdCount(s,thr);
+    }
+
 }
