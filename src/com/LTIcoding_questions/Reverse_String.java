58d523358e4c16b47612c2fc1c439471d898ae4b
mansisgit <mancc81@gmail.com>
2025-09-11 18:28:28 +0530

    done

---
+++ src/com/LTIcoding_questions/Reverse_String.java
@@ -0,0 +1,22 @@
+package com.LTIcoding_questions;
+
+public class Reverse_String {
+
+    public static String revString(String s){
+        String[] words = s.split(" ");
+        StringBuilder rev = new StringBuilder();
+
+        for(String word : words){
+            //new String for reverse word and then append it
+            rev.append(new StringBuilder(word).reverse());
+            rev.append(" ");
+        }
+
+        return rev.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "Mansi Chate";
+        System.out.println(revString(s));
+    }
+}
