6862e50fbb3eb0ec39656d687c7e6808f144201e
mansisgit <mancc81@gmail.com>
2025-09-11 19:07:48 +0530

    done

--- src/com/LTIcoding_questions/Second_Largest.java
+++ src/com/LTIcoding_questions/Second_Largest.java
@@ -1,4 +1,21 @@
 package com.LTIcoding_questions;
 
 public class Second_Largest {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,5,6,7,9};
+        System.out.println(secondLargest(arr));
+    }
+
+    public static int secondLargest(int[] arr){
+        int first = Integer.MIN_VALUE,second = Integer.MAX_VALUE;
+        for(int x: arr){
+            if(x>first){
+                second=first;
+                first=x;
+            }else if(x>second && x != first){
+                second = x;
+            }
+        }
+        return second;
+    }
 }
