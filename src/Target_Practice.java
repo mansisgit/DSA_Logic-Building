d44b106c7a48eecd54e645c3922b192b583cbda7
mansisgit <mancc81@gmail.com>
2025-09-16 10:17:25 +0530

    done

--- src/Target_Practice.java
+++ src/Target_Practice.java
@@ -1,10 +1,6 @@
-
 import java.util.*;
-
-
 public class Target_Practice {
     public static void main(String[] args) throws java.lang.Exception {
-
         int[][] scor_mat = {
                 {1,1,1,1,1,1,1,1,1,1},
                 {1,2,2,2,2,2,2,2,2,1},
@@ -16,7 +12,6 @@ public class Target_Practice {
                 {1,2,3,3,3,3,3,3,2,1},
                 {1,2,2,2,2,2,2,2,2,1},
                 {1,1,1,1,1,1,1,1,1,1}
-
         };
 
         Scanner sc = new Scanner(System.in);
@@ -25,15 +20,16 @@ public class Target_Practice {
         while(tests-- > 0) {
             char[][] mat = new char[10][10];
             for (int i = 0; i < 10; i++) {//10
-                String s = sc.next();
+                String s = sc.next();//row wise input liya hai
                 for(int j =0;j<10;j++){//10
-                    mat[i][j] = s.charAt(j);
+                    mat[i][j] = s.charAt(j);//tracing string
                 }
             }
 
             int score =0;
             for (int i = 0; i < 10; i++) {//10
                 for (int j = 0; j < 10; j++) {//10
+                    //tracing through all points
                     if(mat[i][j] == 'X'){
                        score+=scor_mat[i][j];
                     }
@@ -41,10 +37,5 @@ public class Target_Practice {
             }
             System.out.println(score);
         }
-
-
-
     }
-
-
 }
