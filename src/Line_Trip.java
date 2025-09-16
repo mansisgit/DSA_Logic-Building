1a4f81ce23af3fa4e12eb54b4b6079a089f441b2
mansisgit <mancc81@gmail.com>
2025-09-16 11:49:10 +0530

    done

--- src/Line_Trip.java
+++ src/Line_Trip.java
@@ -1,23 +1,29 @@
-
-
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
-
 public class Line_Trip {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("No of test cases:");
+        //System.out.println("No of test cases:");
         int tests = sc.nextInt();
-        int gasStations = sc.nextInt();
-        int oneway = sc.nextInt();
-        int[] station = new int[gasStations];
 
-        for(int i =0;i<gasStations;i++){
-            station[i] = sc.nextInt();
+        while (tests-->0) {
+
+            int gasStations = sc.nextInt();
+            int total_distance = sc.nextInt();
+            int[] station = new int[gasStations];
+
+            for (int i = 0; i < gasStations; i++) {
+                station[i] = sc.nextInt();
+            }
+
+            int max =station[0];
+
+            for (int i = 1; i < gasStations; i++) {
+                int dis = station[i] - station[i-1];
+                max = Math.max(dis, max);
+            }
+            max = Math.max(max, 2 * (total_distance - station[gasStations - 1]));
+            System.out.println(max);
         }
-        
-
     }
 
 }
