f5dbefc6fa29ce0c9e1477881872bc2758262406
mansisgit <mancc81@gmail.com>
2025-09-19 23:39:11 +0530

    done

---
+++ src/LTIMindtree_codes/Multithreading.java
@@ -0,0 +1,22 @@
+package LTIMindtree_codes;
+
+public class Multithreading {
+     static class Counter{
+        int count =0;
+        void increament(){
+            count++;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException{
+        Counter c = new Counter();
+        Thread t1 = new Thread(()->{for(int i =0;i<1000;i++) c.increament();});
+        Thread t2 = new Thread(()->{for(int i =0;i<1000;i++) c.increament();});
+
+        t1.start();
+        t2.start();
+        t1.join(); t2.join();
+        System.out.println(c.count);
+    }
+
+}
