37b01cc670344f179eb262aebc254dae96a4a6ac
mansisgit <mancc81@gmail.com>
2025-09-21 22:11:48 +0530

    done

--- src/BuyingTorches.java
+++ src/BuyingTorches.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
 
 public class BuyingTorches {
+
+    public static long cell_division(long a , long b){
+        return (a+b-1)/b;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int tests = sc.nextInt();
         while (tests-->0){
 
-            int Sticks_by_1_stick = sc.nextInt();
-            int Sticks_for_1_coal = sc.nextInt();
-            int torches = sc.nextInt();
-            int trade_count = 0;
+            long Sticks_initial= sc.nextInt();
+            long Sticks_for_1_coal = sc.nextInt();
+            long torches = sc.nextInt();
 
-            int total_sticks = torches + (torches*Sticks_for_1_coal);
+            long Sticks_gained = Sticks_initial-1;//trade 1
+            long Sticks_needed = Sticks_for_1_coal*torches + torches-1;
 
+            long trade = 0;
+            trade = cell_division(Sticks_needed,Sticks_gained);
+            trade+=torches;//trade type 2;
+
+            System.out.println(trade);
 
         }
+
     }
 }
